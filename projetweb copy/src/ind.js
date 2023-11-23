43b371ae28a41ed4800f5fc0bbaae2241713cf39
matts890 <143110243+matts890@users.noreply.github.com>
2023-11-23 16:17:16 +0200

    Delete projetweb copy directory

--- projetweb copy/src/ind.js
+++
@@ -1,4 +0,0 @@
-export * from "./Home"
-export * from "./About"
-export * from "./Testomonial"
-export * from "./H"
--- projetweb copy/src/pages/Cart.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Menu() {
-  return (
-    <h1>Cart</h1>
-  )
-}
